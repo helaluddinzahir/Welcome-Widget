92ff32532fee0464d388643b4dccc704a5dddebe
Gurjyot <gurjyottheman@gmail.com>
2016-12-01 01:30:52 +0530

    Add files via upload

---
+++ qa-welcome-widget.js
@@ -0,0 +1,8 @@
+function myFunction() {
+			    	var x = document.getElementById("ww-myTopnav");
+			    	if (x.className === "ww-topnav") {
+			        x.className += " responsive";
+			    	} else {
+			        x.className = "ww-topnav";
+			    	}
+				}
